d50e81366812b75445083ec54c6704b35fb2d859
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-07 12:31:45 +0300

    Update App.py

--- App.py
+++ App.py
@@ -69,9 +69,8 @@ def plot_relative_crime_by_religion_and_group(df, data, selected_group):
 
     # Ensure Quarter is treated as a categorical variable with a specific order
     merged_df['Quarter'] = pd.Categorical(merged_df['Quarter'], ordered=True, categories=sorted(df['Quarter'].unique()))
-    # Define the desired order for the 'Religious level' column
-    religious_order = ['חילונים', 'מסורתיים', 'דתיים', 'חרדים']
-    merged_df['Religious level'] = pd.Categorical(merged_df['Religious level'], categories=religious_order, ordered=True)
+    # Sort the 'Religious level' column lexicographically
+    merged_df['Religious level'] = pd.Categorical(merged_df['Religious level'], categories=sorted(merged_df['Religious level'].unique(), key=lambda x: x.encode('utf-8')), ordered=True)
     if selected_group == 'All':
         # Compute the total number of crimes for each crime group and quarter
         total_crimes_per_group = merged_df.groupby(['StatisticCrimeGroup', 'Quarter'])['TikimSum_original'].sum().reset_index()
@@ -103,7 +102,7 @@ def plot_relative_crime_by_religion_and_group(df, data, selected_group):
     else:
         # Filter the dataframe by selected crime group
         filtered_df = merged_df[merged_df['StatisticCrimeGroup'] == selected_group]
-        filtered_df['Religious level'] = pd.Categorical(filtered_df['Religious level'], categories=religious_order, ordered=True)
+
         # Compute the total number of crimes for each crime type and quarter within the selected crime group
         total_crimes_per_type = filtered_df.groupby(['StatisticCrimeType', 'Quarter'])['TikimSum_original'].sum().reset_index()
         total_crimes_per_type.columns = ['StatisticCrimeType', 'Quarter', 'TotalTikimSum']
@@ -131,7 +130,7 @@ def plot_relative_crime_by_religion_and_group(df, data, selected_group):
                      facet_col_wrap=4,
                      height=2500,  # Set the height to fit the page
                      facet_row_spacing=0.05)  # Adjust row spacing if needed
-    
+
     # Update layout to show x-axis in all facets
     fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True, title_text='אחוז הפשיעה'))
     fig.update_layout(title_x=0.45)
@@ -149,4 +148,3 @@ selected_group = st.selectbox('Select Crime Group', crime_groups)
 
 # Call the function to plot the chart
 plot_relative_crime_by_religion_and_group(df, data, selected_group)
-
