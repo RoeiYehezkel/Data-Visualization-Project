136a14e0383bc6ec88973d273c60e4f6070a414c
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-08 18:26:57 +0300

    Update App.py

--- App.py
+++ App.py
@@ -29,19 +29,7 @@ df = pd.read_csv(preprocessed_data)
 
 
 # Custom CSS to set text direction to right-to-left
-st.markdown(
-    """
-    <style>
-    .rtl-text {
-        direction: rtl;
-        text-align: right;
-    }
-    </style>
-    """,
-    '<h1 class="rtl-text">כיצד משתנה היקף הפשיעה בישראל בהתאם לאזורים גיאוגרפיים שונים ולתקופות זמן שונות?</h1>',
-
-    unsafe_allow_html=True
-)
+st.markdown('<h1 class="rtl-text">כיצד משתנה היקף הפשיעה בישראל בהתאם לאזורים גיאוגרפיים שונים ולתקופות זמן שונות?</h1>',unsafe_allow_html=True)
 
 
 selected_district = st.selectbox("Select Police District", g['PoliceDistrict'].unique())
