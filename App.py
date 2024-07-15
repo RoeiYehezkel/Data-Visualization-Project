add1bb5e4e2f106dfbe07cc6e25bf17d80bb88ca
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-15 20:20:11 +0300

    important events

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-import plotly.express as px
+כimport plotly.express as px
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -69,6 +69,8 @@ fig_all_districts.update_layout(
     legend=dict(font=dict(size=18))
 )
 
+fig_all_districts.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה ראשון', annotation_position='top')
+fig_all_districts.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שני', annotation_position='top')
 fig_all_districts.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שלישי', annotation_position='top')
 fig_all_districts.add_vline(x=13, line=dict(dash='dash', color='white'), annotation_text='שומר החומות', annotation_position='top')
 
@@ -112,6 +114,10 @@ else:
         ), hoverlabel=dict(font_size=20),
         legend=dict(font=dict(size=20))
     )
+    fig.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה ראשון', annotation_position='top')
+    fig.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שני', annotation_position='top')
+    fig.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שלישי', annotation_position='top')
+    fig.add_vline(x=13, line=dict(dash='dash', color='white'), annotation_text='שומר החומות', annotation_position='top')
     fig.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=20)))
     fig.for_each_xaxis(lambda xaxis: xaxis.update(tickfont=dict(size=20)))
     fig.update_traces(
