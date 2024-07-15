74a7b2ce993228780c1340a2ff7414869352f922
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-15 20:48:39 +0300

    Update App.py

--- App.py
+++ App.py
@@ -39,11 +39,15 @@ st.markdown(
         gap: 10px;
         margin-top: 10px;
     }
-    .stSelectbox div[data-baseweb="select"] > div {
+    .stSelectbox div[data-baseweb="select"] {
         direction: rtl;
         text-align: right;
     }
     .stSelectbox div[data-baseweb="select"] .css-1uccc91-singleValue {
+        direction: rtl;
+        text-align: right;
+    }
+    .stSelectbox div[data-baseweb="select"] .css-1n7v3ny-option {
         direction: rtl;
         text-align: right;
     }
@@ -135,6 +139,10 @@ else:
         ), hoverlabel=dict(font_size=20),
         legend=dict(font=dict(size=20))
     )
+    fig.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר ראשון', annotation_position='top')
+    fig.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר שני', annotation_position='top')
+    fig.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר שלישי', annotation_position='top')
+    fig.add_vline(x=13, line=dict(dash='dash', color='white'), annotation_text='שומר החומות', annotation_position='top')
     fig.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=18)))
     fig.for_each_xaxis(lambda xaxis: xaxis.update(tickfont=dict(size=18)))
     fig.update_traces(
