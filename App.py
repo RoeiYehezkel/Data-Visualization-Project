42bd1efdd9492aad0333005e0b799f092c8adb71
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-15 15:34:00 +0300

    theme

--- App.py
+++ App.py
@@ -65,10 +65,6 @@ fig_all_districts.update_traces(
 # Dropdown with an additional "כלל המחוזות" option
 options = ["כלל המחוזות"] + list(g['PoliceDistrict'].unique())
 selected_district = st.selectbox("בחר את מחוז המשטרה", options)
-fig.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=15)))
-fig.for_each_xaxis(lambda xaxis: xaxis.update(tickfont=dict(size=15)))
-fig.update_layout(hoverlabel=dict(font_size=15), legend=dict(font=dict(size=15)))
-    
 if selected_district == "כלל המחוזות":
     st.plotly_chart(fig_all_districts)
 else:
