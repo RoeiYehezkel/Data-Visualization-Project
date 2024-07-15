514fd6c113965d04ce8f128462a2acbeaa161808
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-15 20:21:58 +0300

    important events

--- App.py
+++ App.py
@@ -69,9 +69,9 @@ fig_all_districts.update_layout(
     legend=dict(font=dict(size=18))
 )
 
-fig_all_districts.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה ראשון', annotation_position='top')
-fig_all_districts.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שני', annotation_position='top')
-fig_all_districts.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שלישי', annotation_position='top')
+fig_all_districts.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר ראשון', annotation_position='top')
+fig_all_districts.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר שני', annotation_position='top')
+fig_all_districts.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר שלישי', annotation_position='top')
 fig_all_districts.add_vline(x=13, line=dict(dash='dash', color='white'), annotation_text='שומר החומות', annotation_position='top')
 
 fig_all_districts.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=15)))
@@ -114,9 +114,9 @@ else:
         ), hoverlabel=dict(font_size=20),
         legend=dict(font=dict(size=20))
     )
-    fig.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה ראשון', annotation_position='top')
-    fig.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שני', annotation_position='top')
-    fig.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר קורונה שלישי', annotation_position='top')
+    fig.add_vline(x=9, line=dict(dash='dash', color='white'), annotation_text='סגר ראשון', annotation_position='top')
+    fig.add_vline(x=11, line=dict(dash='dash', color='white'), annotation_text='סגר שני', annotation_position='top')
+    fig.add_vline(x=12, line=dict(dash='dash', color='white'), annotation_text='סגר שלישי', annotation_position='top')
     fig.add_vline(x=13, line=dict(dash='dash', color='white'), annotation_text='שומר החומות', annotation_position='top')
     fig.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=20)))
     fig.for_each_xaxis(lambda xaxis: xaxis.update(tickfont=dict(size=20)))
