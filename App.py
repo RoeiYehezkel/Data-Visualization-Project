ef65faf71822f25b05ee36c81bb410584d8da85a
RoeiYehezkel <97667421+RoeiYehezkel@users.noreply.github.com>
2024-07-16 10:05:55 +0300

    Update App.py

--- App.py
+++ App.py
@@ -88,7 +88,7 @@ fig_all_districts.add_vline(x=13, line=dict(dash='dash', color='white'), annotat
 fig_all_districts.for_each_yaxis(lambda yaxis: yaxis.update(tickfont=dict(size=15)))
 fig_all_districts.for_each_xaxis(lambda xaxis: xaxis.update(tickfont=dict(size=15)))
 fig_all_districts.update_traces(
-    hovertemplate='%{x}<br>סכום התיקים=%{y:,}<br>שינוי מהתקופה הקודמת=%{PercentIncrease:.2f}%%'
+    hovertemplate='%{x}<br>סכום התיקים=%{y:,}<br>שינוי מהתקופה הקודמת=%{PercentIncrease:.2f}'
 )
 
 # Dropdown with an additional "כלל המחוזות" option
